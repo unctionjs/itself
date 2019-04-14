414e405058244b98ecceeb6f6ace933aacd769ef
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-04-13 21:01:44 -0700

    Autolinting

--- test.js
+++ test.js
@@ -1,31 +1,31 @@
 /* eslint-disable flowtype/require-return-type */
-import {test} from "tap"
+import {test} from "tap";
 
-import itself from "./index"
+import itself from "./index";
 
 test(({same, end}) => {
   same(
     itself("a"),
     "a"
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test(({same, end}) => {
   same(
     itself(true),
     true
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test(({same, end}) => {
   same(
     itself(null),
     null
-  )
+  );
 
-  end()
-})
+  end();
+});
